05845ec3bef3ca18f60f4d608e8546ba1cc97671
Ruben D. <ruben.dedecker@ugent.be>
2023-11-28 14:29:29 +0100

    Added n3.js branch install

---
+++ poddatainterface/api.ts
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+const port = 3456
+
+import * as n3  from 'n3';
+
+import * as crypto from 'crypto';
+
+import { signContent } from "../packaging/createSignedPackage";
+
+
+async function run() {
+
+    const govid = 'http://localhost:3456/flandersgov/id'
+    const govdata = 'http://localhost:3456/flandersgov/data/'
+
+    let keypair = await crypto.subtle.generateKey(
+        {
+            name: "ECDSA",
+            namedCurve: "P-384",
+        },
+        true,
+        ["sign", "verify"]
+    );
+
+    let publicKeyRaw = await crypto.subtle.exportKey("raw", keypair.publicKey)
+    let publicKeyString = Buffer.from(publicKeyRaw).toString('base64')
+
+    //@ts-ignore
+    app.get('/flandersgov/endpoint/dob', async (req, res) => {
+        let id = req.query.id
+        console.log(id)
+
+        
+        let bdateTriple = n3.DataFactory.quad(
+            n3.DataFactory.namedNode(id), 
+            n3.DataFactory.namedNode("https://www.w3.org/2006/vcard/ns#bday"), 
+            n3.DataFactory.literal(new Date('2000-01-01T10:00:00').toISOString(), new n3.NamedNode("http://www.w3.org/2001/XMLSchema#dateTime"))
+        );
+
+        let tripleString : string = await new Promise((resolve, reject) => {
+            const writer = new n3.Writer({}); // Create a writer which uses `c` as a prefix for the namespace `http://example.org/cartoons#`
+            writer.addQuad(bdateTriple)
+            writer.end((error: any, result: any) => { resolve(result) });
+        })
+
+        
+        console.log('triple', tripleString)
+
+        let content = await signContent(tripleString, govid, keypair.privateKey)
+
+        res.send(content)
+    })
+
+
+    //@ts-ignore
+    app.get('/flandersgov/id', async (req, res) => {
+
+        let webId = 
+`@prefix foaf: <http://xmlns.com/foaf/0.1/>.
+
+<${govid}> a foaf:PublicInstance;
+    foaf:name "Flanders Government"@en;
+    foaf:homepage <https://www.vlaanderen.be>;
+    <http://www.w3.org/ns/auth/cert#key>  "${publicKeyString}".
+`
+        res.send(webId)
+    })
+
+
+    app.listen(port, () => {
+        console.log(
+`Running government birthdate API system
+
+identity document:
+http://localhost:3456/flandersgov/id
+
+birthdate endpoint call:
+http://localhost:3456/flandersgov/endpoint/dob?id=<webid>`
+        )
+    })
+
+}
+
+
+run()
