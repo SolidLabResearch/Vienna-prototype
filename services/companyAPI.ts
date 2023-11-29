372dd1661145320626c4e30f25343b80b2ccfb28
Ruben D. <ruben.dedecker@ugent.be>
2023-11-29 13:23:54 +0100

    Added extra endpoints for testdata generation

--- services/companyAPI.ts
+++ services/companyAPI.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express()
-const port = 3456
+const port = 3457
 
 import * as n3  from 'n3';
 
@@ -11,7 +11,7 @@ import { signContent } from "../packaging/createSignedPackage";
 
 async function run() {
 
-    const govid = `http://localhost:${port}/flandersgov/id`
+    const compid = `http://localhost:${port}/company/id`
 
     let keypair = await crypto.subtle.generateKey(
         {
@@ -25,7 +25,7 @@ async function run() {
     let publicKeyRaw = await crypto.subtle.exportKey("raw", keypair.publicKey)
     let publicKeyString = Buffer.from(publicKeyRaw).toString('base64')
 
-    app.get('/flandersgov/endpoint/dob', async (req, res) => {
+    app.get('/company/endpoint/licensekey', async (req, res) => {
         let id = req.query.id
         
         
@@ -34,34 +34,31 @@ async function run() {
         }
 
         
-        let bdateTriple = n3.DataFactory.quad(
+        let triple = n3.DataFactory.quad(
             n3.DataFactory.namedNode(id), 
-            n3.DataFactory.namedNode("https://www.w3.org/2006/vcard/ns#bday"), 
-            n3.DataFactory.literal(new Date('2000-01-01T10:00:00').toISOString(), n3.DataFactory.namedNode("http://www.w3.org/2001/XMLSchema#dateTime"))
+            n3.DataFactory.namedNode("https://www.company.be/namespace/licensekey"), 
+            n3.DataFactory.literal((Math.random() + 1).toString(36).substring(2))
         );
 
         let tripleString : string = await new Promise((resolve, reject) => {
             const writer = new n3.Writer({}); // Create a writer which uses `c` as a prefix for the namespace `http://example.org/cartoons#`
-            writer.addQuad(bdateTriple)
+            writer.addQuad(triple)
             writer.end((error: any, result: any) => { resolve(result) });
         })
 
-        
-        console.log('triple', tripleString)
-
-        let content = await signContent(tripleString, govid, keypair.privateKey)
+        let content = await signContent(tripleString, compid, keypair.privateKey)
 
         res.status(200).contentType('text/n3').send(content)
     })
 
-    app.get('/flandersgov/id', async (req, res) => {
+    app.get('/company/id', async (req, res) => {
 
         let webId = 
 `@prefix foaf: <http://xmlns.com/foaf/0.1/>.
 
-<${govid}> a foaf:PublicInstance;
-    foaf:name "Flanders Government"@en;
-    foaf:homepage <https://www.vlaanderen.be>;
+<${compid}> a foaf:Company;
+    foaf:name "Company X"@en;
+    foaf:homepage <https://www.companyX.be>;
     <http://www.w3.org/ns/auth/cert#key>  "${publicKeyString}".
 `
         res.status(200).contentType('text/turtle').send(webId)
@@ -70,13 +67,13 @@ async function run() {
 
     app.listen(port, () => {
         console.log(
-`Running government birthdate API system
+`Running Company API
 
 identity document URI:
-${govid}
+${compid}
 
-birthdate endpoint URI:
-http://localhost:${port}/flandersgov/endpoint/dob?id=<webid>`
+licensekey endpoint URI:
+http://localhost:${port}/company/endpoint/licensekey?id=<webid>`
         )
     })
 
---
+++ services/flandersAPI.ts
@@ -0,0 +1,163 @@
+import express from 'express';
+const app = express()
+const port = 3456
+
+import * as n3  from 'n3';
+
+import * as crypto from 'crypto';
+
+import { signContent } from "../packaging/createSignedPackage";
+
+
+async function run() {
+
+    const govid = `http://localhost:${port}/flandersgov/id`
+
+    let keypair = await crypto.subtle.generateKey(
+        {
+            name: "ECDSA",
+            namedCurve: "P-384",
+        },
+        true,
+        ["sign", "verify"]
+    );
+
+    let publicKeyRaw = await crypto.subtle.exportKey("raw", keypair.publicKey)
+    let publicKeyString = Buffer.from(publicKeyRaw).toString('base64')
+
+    app.get('/flandersgov/endpoint/dob', async (req, res) => {
+        let id = req.query.id
+        
+        
+        if (typeof id !== 'string') {
+            return res.status(500).send("id must be a string")
+        }
+
+        
+        let bdateTriple = n3.DataFactory.quad(
+            n3.DataFactory.namedNode(id), 
+            n3.DataFactory.namedNode("https://www.w3.org/2006/vcard/ns#bday"), 
+            n3.DataFactory.literal(new Date('2000-01-01T10:00:00').toISOString(), n3.DataFactory.namedNode("http://www.w3.org/2001/XMLSchema#dateTime"))
+        );
+
+        let tripleString : string = await new Promise((resolve, reject) => {
+            const writer = new n3.Writer({}); // Create a writer which uses `c` as a prefix for the namespace `http://example.org/cartoons#`
+            writer.addQuad(bdateTriple)
+            writer.end((error: any, result: any) => { resolve(result) });
+        })
+
+        
+        console.log('triple', tripleString)
+
+        let content = await signContent(tripleString, govid, keypair.privateKey)
+
+        res.status(200).contentType('text/n3').send(content)
+    })
+
+    app.get('/flandersgov/endpoint/name', async (req, res) => {
+        let id = req.query.id
+        
+        if (typeof id !== 'string') {
+            return res.status(500).send("id must be a string")
+        }
+
+        
+        let bdateTriple = n3.DataFactory.quad(
+            n3.DataFactory.namedNode(id), 
+            n3.DataFactory.namedNode("http://xmlns.com/foaf/0.1/name"), 
+            n3.DataFactory.literal("Bob The Builder")
+        );
+
+        let tripleString : string = await new Promise((resolve, reject) => {
+            const writer = new n3.Writer({}); // Create a writer which uses `c` as a prefix for the namespace `http://example.org/cartoons#`
+            writer.addQuad(bdateTriple)
+            writer.end((error: any, result: any) => { resolve(result) });
+        })
+
+        let content = await signContent(tripleString, govid, keypair.privateKey)
+
+        res.status(200).contentType('text/n3').send(content)
+    })
+
+
+    app.get('/flandersgov/endpoint/address', async (req, res) => {
+        let id = req.query.id
+        
+        if (typeof id !== 'string') {
+            return res.status(500).send("id must be a string")
+        }
+
+        
+        let triples = [
+            n3.DataFactory.quad(
+                n3.DataFactory.namedNode(id), 
+                n3.DataFactory.namedNode("https://www.w3.org/2006/vcard/ns#hasAddress"), 
+                n3.DataFactory.blankNode("address"),
+            ), n3.DataFactory.quad(
+                n3.DataFactory.blankNode("address"),
+                n3.DataFactory.namedNode("https://www.w3.org/2006/vcard/ns#street-address"), 
+                n3.DataFactory.literal("Technologiepark-Zwijnaarde 122 ")
+            ),  n3.DataFactory.quad(
+                n3.DataFactory.blankNode("address"),
+                n3.DataFactory.namedNode("https://www.w3.org/2006/vcard/ns#locality"), 
+                n3.DataFactory.literal("Ghent")
+            ), n3.DataFactory.quad(
+                n3.DataFactory.blankNode("address"),
+                n3.DataFactory.namedNode("https://www.w3.org/2006/vcard/ns#postal-code"), 
+                n3.DataFactory.literal(9052)
+            ), n3.DataFactory.quad(
+                n3.DataFactory.blankNode("address"),
+                n3.DataFactory.namedNode("https://www.w3.org/2006/vcard/ns#country-name"), 
+                n3.DataFactory.literal("Belgium")
+            ),
+        ]
+
+        let tripleString : string = await new Promise((resolve, reject) => {
+            const writer = new n3.Writer({}); // Create a writer which uses `c` as a prefix for the namespace `http://example.org/cartoons#`
+            writer.addQuads(triples)
+            writer.end((error: any, result: any) => { resolve(result) });
+        })
+
+        let content = await signContent(tripleString, govid, keypair.privateKey)
+
+        res.status(200).contentType('text/n3').send(content)
+    })
+
+
+    app.get('/flandersgov/id', async (req, res) => {
+
+        let webId = 
+`@prefix foaf: <http://xmlns.com/foaf/0.1/>.
+
+<${govid}> a foaf:PublicInstance;
+    foaf:name "Flanders Government"@en;
+    foaf:homepage <https://www.vlaanderen.be>;
+    <http://www.w3.org/ns/auth/cert#key>  "${publicKeyString}".
+`
+        res.status(200).contentType('text/turtle').send(webId)
+    })
+
+
+    app.listen(port, () => {
+        console.log(
+`Running government birthdate API system
+
+identity document URI:
+${govid}
+
+
+name endpoint URI:
+http://localhost:${port}/flandersgov/endpoint/name?id=<webid>
+
+birthdate endpoint URI:
+http://localhost:${port}/flandersgov/endpoint/dob?id=<webid>
+
+address endpoint URI:
+http://localhost:${port}/flandersgov/endpoint/address?id=<webid>`
+        )
+    })
+
+}
+
+
+run()
